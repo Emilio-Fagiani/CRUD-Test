065875124bfc3ba6d4743de11dae8874e30472cc
Emilio <emi00@gmail.com>
2022-11-28 11:29:50 +0100

    feat: First commit

---
+++ src/test/java/it/develhope/CRUD/Test/CrudTestApplicationTests.java
@@ -0,0 +1,13 @@
+package it.develhope.CRUD.Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
